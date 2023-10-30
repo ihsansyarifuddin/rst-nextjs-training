0acaf5904f1a4217853b55a7fcb75a00b88ef577
Ihsan Syarifuddin <ihsan.syarifuddin@rschooltoday.com>
2023-10-30 22:35:17 +0700

    edit

--- src/middleware.ts
+++ src/middleware.ts
@@ -30,7 +30,16 @@ export function middleware(request: NextRequest) {
         return NextResponse.redirect(new URL('/login', request.url))
     }
 
-    return NextResponse.next()
+    const res = NextResponse.next()
+    res.headers.append('Access-Control-Allow-Credentials', "true")
+    res.headers.append('Access-Control-Allow-Origin', '*') // replace this your actual origin
+    res.headers.append('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
+    res.headers.append(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+
+    return res
 }
 
 export const config = {
