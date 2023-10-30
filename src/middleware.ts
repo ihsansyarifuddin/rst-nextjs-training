471283c437b60c42b378de913db3bc5080ea8985
Ihsan Syarifuddin <ihsan.syarifuddin@rschooltoday.com>
2023-10-30 23:08:19 +0700

    Revert "edit"
    This reverts commit 0acaf5904f1a4217853b55a7fcb75a00b88ef577.

--- src/middleware.ts
+++ src/middleware.ts
@@ -30,16 +30,7 @@ export function middleware(request: NextRequest) {
         return NextResponse.redirect(new URL('/login', request.url))
     }
 
-    const res = NextResponse.next()
-    res.headers.append('Access-Control-Allow-Credentials', "true")
-    res.headers.append('Access-Control-Allow-Origin', '*') // replace this your actual origin
-    res.headers.append('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
-    res.headers.append(
-        'Access-Control-Allow-Headers',
-        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    )
-
-    return res
+    return NextResponse.next()
 }
 
 export const config = {
